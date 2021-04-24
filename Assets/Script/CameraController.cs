fc5dbfe78eaacfbe0db873aeb974ba9a9d7337c5
nagashima0112 <mski4649@icloud.com>
2021-04-24 12:40:53 +0900

    unitychanを動かす カメラが追従するようにする

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //MainCamera ゲームオブジェクトにアタッチされている Camera コンポーネントの情報を代入する
+    [SerializeField]
+    private Camera mainCamera;
+
+    //FPSCamera ゲームオブジェクトにアタッチされている Camera コンポーネントの情報を代入する
+    [SerializeField]
+    private Camera FPSCamera;
+
+    //「bool」は「true」か「false」の二択の情報を扱うことができる
+    //ボタンの操作を行った際に、使用するカメラを切り替える制御を行っている
+    public bool mainCameraON = true;
+
+    //MainカメラがONの時にmainListener変数で音を拾う
+    //AudioListener コンポーネントを制御して切り替えるため
+    //MainCamera (FPSCamera) ゲームオブジェクトをアサインし、
+    //そのゲームオブジェクトにアタッチされている AudioListener 型の変数を代入し、制御するための変数
+    [SerializeField]
+    private AudioListener mainListener;
+
+    //FPSカメラがONの時にFPSListener変数で音を拾う
+    //AudioListener コンポーネントを制御して切り替えるため
+    //MainCamera (FPSCamera) ゲームオブジェクトをアサインし、
+    //そのゲームオブジェクトにアタッチされている AudioListener 型の変数を代入し、制御するための変数
+    [SerializeField]
+    private AudioListener FPSListener;
+
+    void Start()
+    {
+        //Mainカメラをオンにする
+        mainCamera.enabled = true;
+
+        //FPSカメラをオフにする
+        FPSCamera.enabled = false;
+
+        //MainListenerをオンにする
+        mainListener.enabled = true;
+
+        //FPSListenerをオフにする
+        FPSListener.enabled = false;
+    }
+
+    void Update()
+    {
+        //キーボードのCボタンが押された時、MainカメラがONであるなら
+        //メインカメラを使用している場合には true、使用していない場合には false
+        if (Input.GetKeyDown(KeyCode.C) && mainCameraON == true)
+        {
+            //Mainカメラをオフにする
+            mainCamera.enabled = false;
+
+            //FPSカメラをオンにする
+            FPSCamera.enabled = true;
+
+            //Mainカメラをオフにする
+            mainCameraON = false;
+
+            //MainListenerをオフにする
+            mainListener.enabled = false;
+
+            //FPSListenerをオンにする
+            FPSListener.enabled = true;
+        }
+
+        //キーボードのCボタンが押された時、かつ、MainカメラがOFFであるなら
+        //メインカメラを使用している場合には true、使用していない場合には false
+        else if (Input.GetKeyDown(KeyCode.C) && mainCameraON == false)
+        {
+            //Mainカメラをオンにする
+            mainCamera.enabled = true;
+
+            //FPSカメラをオフにする
+            FPSCamera.enabled = false;
+
+            //Mainカメラをオンにする
+            mainCameraON = true;
+
+            //MainListenerをオンにする
+            mainListener.enabled = true;
+
+            //FPSListenerをオフにする
+            FPSListener.enabled = false;
+        }
+    }
+}
---
+++ Assets/Script/UnitychanMove.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitychanMove : MonoBehaviour
+{
+    [Header("戦車の動く速さ")]
+    public float moveSpeed;
+
+    [Header("戦車の旋回する速さ")]
+    public float turnSpeed;
+
+    private Rigidbody rb;
+
+    private float movementInputValue;
+
+    private float turnInputValue;
+
+    //private float defaultMoveSpped;
+
+    //インスペクターで操作する時にどこの操作をする部分か分かるように付ける
+    //[Header("移動速度変更の持続時間")]
+    //public float duration;
+    //public float timer = 0f;
+    //public bool isChengSpped;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //現在のスピードを保存
+        //defaultMoveSpped = moveSpeed;
+    }
+
+    void Update()
+    {
+        TankMove();
+        TankTurn();
+
+        //移動速度が変更中だったら
+        //if (isChengSpped == true)
+        {
+            //timer -= Time.deltaTime;
+            //if (timer < 0)
+            {
+                //timer = 0;
+                //変更時間が終了したから速度を戻す
+                //DefaltMoveSpped();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 前進・後退のメソッド
+    /// </summary>
+    void TankMove()
+    {
+        movementInputValue = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * movementInputValue * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    /// <summary>
+    /// 旋回のメソッド
+    /// </summary>
+    void TankTurn()
+    {
+        turnInputValue = Input.GetAxis("Horizontal");
+        float turn = turnInputValue * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+
+    /// <summary>
+    /// 移動速度を変更
+    /// </summary>
+    /// <param name="newMoveSpped"></param>
+    //public void ChengMoveSpped(float newMoveSpped)
+    //{
+        //移動速度を変更中の状態にする
+        //isChengSpped = true;
+        //変更時間をセット
+        //timer = duration;
+        //新しいスピードに変更
+        //moveSpeed = newMoveSpped;
+        //Debug.Log("移動スピード変更:" + moveSpeed);
+    //}
+
+    /// <summary>
+    /// 移動速度を元に戻す
+    /// </summary>
+    //public void DefaltMoveSpped()
+    //{
+        //移動速度を変更中ではない状態にする
+        //isChengSpped = false;
+        //moveSpeed = defaultMoveSpped;
+        //Debug.Log("移動スピード変更:" + moveSpeed);
+    //}
+}
