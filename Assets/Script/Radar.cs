8c7097370cac351feeb1e5df43751e931e2b8cd9
nagashima0112 <mski4649@icloud.com>
2021-05-02 17:43:08 +0900

    敵の弾の修正

--- Assets/Script/Radar.cs
+++ Assets/Script/Radar.cs
@@ -7,15 +7,16 @@ public class Radar : MonoBehaviour
     [SerializeField]
     private Transform target;
 
-    private void Start()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    //private void Start()
+    //{
+        //target = GameObject.FindGameObjectWithTag("Player").transform;
+    //}
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+
             if(target != null)
             {
                 transform.root.LookAt(target);
