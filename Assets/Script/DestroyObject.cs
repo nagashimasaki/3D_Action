c49f6abbd3b0ef538d47f5719bea2fe3be0f817a
nagashima0112 <mski4649@icloud.com>
2021-05-24 12:07:16 +0900

    アセットのインポート

--- Assets/Script/DestroyObject.cs
+++ Assets/Script/DestroyObject.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class DestroyObject : MonoBehaviour
 {
     //エフェクト1を入れる箱を作る
-    [SerializeField]
-    private GameObject effectPrefab1;
+    //[SerializeField]
+    //private GameObject effectPrefab1;
 
     //エフェクト2を入れる箱を作る
-    [SerializeField]
-    private GameObject effectPrefab2;
+    //[SerializeField]
+    //private GameObject effectPrefab2;
 
     //objectHP 変数にHPの値を入れる
     public int objectHP;
@@ -34,10 +34,10 @@ public class DestroyObject : MonoBehaviour
                 //エフェクト1のプレファブ・ゲームオブジェクトからクローンのゲームオブジェクトを、
                 //このスクリプトがアタッチしている,ゲームオブジェクトの位置にエフェクト1を無回転で生成し、
                 //そのゲームオブジェクトの情報を左辺の effect 変数に代入することで制御を行える状態にする
-                GameObject effect = Instantiate(effectPrefab1, other.transform.position, Quaternion.identity);
+                //GameObject effect = Instantiate(effectPrefab1, other.transform.position, Quaternion.identity);
 
                 //画面に表示しているエフェクト1をを2秒後に破棄する
-                Destroy(effect, 2.0f);
+                //Destroy(effect, 2.0f);
             }
             //HPが０よりも小さい場合には（条件）
             else
@@ -45,10 +45,10 @@ public class DestroyObject : MonoBehaviour
                 //エフェクト2のプレファブ・ゲームオブジェクトからクローンのゲームオブジェクトを、
                 //このスクリプトがアタッチしている,ゲームオブジェクトの位置にエフェクト2を無回転で生成し、
                 //そのゲームオブジェクトの情報を左辺の effect2 変数に代入することで制御を行える状態にする
-                GameObject effect2 = Instantiate(effectPrefab2, other.transform.position, Quaternion.identity);
+                //GameObject effect2 = Instantiate(effectPrefab2, other.transform.position, Quaternion.identity);
 
                 //画面に表示しているエフェクト2を2秒後に破壊する
-                Destroy(effect2, 2.0f);
+                //Destroy(effect2, 2.0f);
 
                 //このスクリプトが付いているオブジェクトを破壊する
                 Destroy(gameObject);
