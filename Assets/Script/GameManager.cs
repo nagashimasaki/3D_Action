5309cfef57afc39e57f527b78b634dc4e0ad7b11
nagashima0112 <mski4649@icloud.com>
2021-05-24 12:20:44 +0900

    ゲームスタート時の空をランダムにする

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private SkyboxChanger skyboxChanger;
+
+    void Awake()
+    {
+        // Skyboxの変更
+        skyboxChanger.ChangeSkybox();
+    }
+}
