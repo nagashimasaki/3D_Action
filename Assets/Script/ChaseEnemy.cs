9089c04720bb9f09b0b4dae4a75a767abf6af0f9
nagashima0112 <mski4649@icloud.com>
2021-05-29 10:55:08 +0900

    修正

--- Assets/Script/ChaseEnemy.cs
+++ Assets/Script/ChaseEnemy.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class ChaseEnemy : MonoBehaviour
 {
+
     //ターゲットにするゲームオブジェクトの情報を入れる箱を作る
     [SerializeField]
     public GameObject target;
@@ -12,11 +13,16 @@ public class ChaseEnemy : MonoBehaviour
     //NavMeshAgent コンポーネントの情報を扱うための変数
     private NavMeshAgent agent;
 
+    private Animator anim;
+
     void Start()
     {
         //NavMeshAgentコンポーネントはゲームオブジェクトを自動的に移動させる情報を持っている
         //agent 変数でNavMeshAgentコンポーネントの情報を使えるようにする
         agent = GetComponent<NavMeshAgent>();
+
+        //アニメーターコンポーネントを使えるようにする
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +32,23 @@ public class ChaseEnemy : MonoBehaviour
         {
             //ターゲットの位置を目的地に設定する
             agent.destination = target.transform.position;
+
+            anim.SetFloat("spped", agent.velocity.sqrMagnitude);
+            ////このゲームオブジェクトの位置が目的地と違う場合
+            //if (transform.position != agent.destination) 
+            //{
+
+            //    //アニメーションを歩いているアニメーションにする
+            //    anim.SetBool("walking", true);
+            //}
+
+            ////目的地に付いている場合
+            //else
+            //{
+
+            //    //アニメーションをaidlの状態にする
+            //    anim.SetBool("walking", false);
+            //}
         }
     }
 }
